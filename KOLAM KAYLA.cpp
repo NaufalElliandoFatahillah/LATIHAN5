cdd15957ef30596e5cfc9409830de9e31a51a64f
Naufal Elliando Fatahillah <92315859+NaufalElliandoFatahillah@users.noreply.github.com>
2021-11-29 08:47:44 +0700

    Add files via upload

---
+++ KOLAM KAYLA.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    float L, r ;
+    float pi = 3.14 ;
+    
+    cin >> r ;
+    L = pi * r * r ;
+    cout << "" << L << endl ;
+    
+    return 0;
+}
